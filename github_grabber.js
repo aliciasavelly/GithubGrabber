7e2e538d5bc092177c8b2f6b40604c72c03eb037
aliciasavelly <alicia.savelly@gmail.com>
2017-09-28 00:45:25 -0700

    work on github grabber

--- github_grabber.js
+++ github_grabber.js
@@ -5,6 +5,33 @@ const http = require('http');
 
 const githubServer = http.createServer((req, res) => {
   if (req.method === 'POST') {
+    let requestBody = '';
+
+    req.on('data', data => {
+      requestBody += data;
+    });
+
+    req.on('end', () => {
+      const username = qs.parse(requestBody).username;
+      const ws = fs.createWriteStream(`./${username}_starred_repos.txt`);
+      const options = buildOptionsObj(username);
+
+      https.get(options, dataStream => {
+        let repoData = '';
+
+        dataStream.on('data', data => { repoData += data });
+
+        dataStream.on('end', () => {
+          const repos = JSON.parse(repoData).map(repo => {
+            return `Repo: ${repo.name}. Stars: ${repo.stargazers_count}.`;
+          }).join('\n');
+
+          ws.write(repos);
+          res.end(repos);
+        });
+      });
+    });
+
     res.end("I'm a post request!");
   } else {
     res.end("Danger, not a post request!");
